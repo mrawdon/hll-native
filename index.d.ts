f1107602fa1ea4cbb8e2ff4cc0dc2520a94c3183
Adam Coll <adamcoll.ac@gmail.com>
2020-10-01 08:59:46 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,7 @@
-declare class HllNative{
+export default class HllNative{
     constructor();
     add(data: string):void;
     merge(hll: HllNative):void;
     cardinality():number;
     deserialize(date:string):void;
-}
-
-declare module "hll-native"{
-    export = HllNative
 }
